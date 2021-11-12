8a470f88e7742cb269297b13f3f17c911f60f403
hide101100 <dashasneg@mail.ru>
2021-11-12 23:30:12 +0500

    add and configure store

--- src/components/Views/Main/index.tsx
+++ src/components/Views/Main/index.tsx
@@ -1,7 +1,19 @@
 import * as React from 'react';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { rootReducer } from '../../../redux/rootReducer';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 const Main = () => {
-	return <></>;
+	return (
+		<Provider store={store}>
+			<div className="app">
+
+			</div>
+		</Provider>
+	);
 };
 
 export default Main;
